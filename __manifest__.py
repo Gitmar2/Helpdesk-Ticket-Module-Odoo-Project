5dc855883b755360f1de85a07c5a42642a53ced9
Gitmar2 <jhomarvaron16@gmail.com>
2026-04-10 08:49:46 +0800

    fix Resolved empty groups.xml error

--- __manifest__.py
+++ __manifest__.py
@@ -1,11 +1,11 @@
 {
-    'name': 'Helpdesk Ticket v1.0',
+    'name': 'Helpdesk Ticket',
     'version': '1.0',
     'summary': 'Basic module',
     'author': 'You',
     'category': 'Tools',
     'depends': ['base'],
-    'data': [],
+    #'data': [],
     'installable': True,
     'application': True,
 }
